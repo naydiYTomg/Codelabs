452be0d012cc4a757d9b4152616f506681fa2e9e
coolestProcaviidae <85066178+genericWindowsUser416@users.noreply.github.com>
2025-03-29 17:35:13 +0300

    Update UserDTO.cs

--- Codelabs.Core/DTOs/UserDTO.cs
+++ Codelabs.Core/DTOs/UserDTO.cs
@@ -2,12 +2,20 @@
 {
     public class UserDTO
     {
-        public int ID { get; set; }
-
-        public Role Role { get; set; }
+        public int Id { get; set; }
 
         public string? Login { get; set; }
 
         public byte[]? Password { get; set; }
+        
+        public string? Name { get; set; }
+
+        public string? Surname { get; set; }
+
+        public Role Role { get; set; }
+
+        public string? Phone { get; set; }
+
+        public string? Email { get; set; }
     }
 }
