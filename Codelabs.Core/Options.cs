ca00ab8ab9a9ee61e739ee40eaf5ded70a585711
Петр Брякин <petr.bryakin@gmail.com>
2025-04-07 20:09:36 +0300

    change ConnectionString

--- Codelabs.Core/Options.cs
+++ Codelabs.Core/Options.cs
@@ -2,5 +2,5 @@ namespace Codelabs.Core;
 
 public static class Options
 {
-    public static readonly string ConnectionString = Environment.GetEnvironmentVariable("CodelabsConnectionString");
+    public static readonly string ConnectionString = Environment.GetEnvironmentVariable("DATABASE_ACCESS");
 }
