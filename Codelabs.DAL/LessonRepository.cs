6ebf067fd9c72af0eab1155d32efa3f41d22fe33
coolestProcaviidae <85066178+genericWindowsUser416@users.noreply.github.com>
2025-04-12 16:53:50 +0300

    (PAGE STUCK ON LOAD FOREVER) updated it

--- Codelabs.DAL/LessonRepository.cs
+++ Codelabs.DAL/LessonRepository.cs
@@ -20,9 +20,28 @@ public class LessonRepository
     public async Task<List<LessonDTO>> GetAllExistingLessonsFromPurchasesByUser(int userID)
     {
         await using var context = new Context();
-        return await context.Purchases
-            .Where(x => x.User.ID == userID)
-            .Select(p => p.Lesson)
-            .ToListAsync();
+        var step1 = context.Purchases.Include(x => x.User);
+
+        var step2 = step1.Where(x => x.User.ID == userID);
+
+        var step3 = step2.Select(p => p.Lesson);
+
+        var step4 = step3.Include(x => x.Author);
+
+        var step5 = step4.Include(x => x.Language);
+
+        var step6 = step5.Where(x => !x.IsDeleted);
+
+        var step7 = await step6.ToListAsync();
+
+        //return await context.Purchases
+        //            .Include(x => x.User)
+        //            .Where(x => x.User.ID == userID)
+        //            .Select(p => p.Lesson)
+        //            .Include(x => x.Author)
+        //            .Include(x => x.Language)
+        //            .Where(x => !x.IsDeleted)
+        //            .ToListAsync();
+        return step7;
     }
 }
