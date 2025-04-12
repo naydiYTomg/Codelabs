f5c60b0714701e0c3178d1f7105090651e7563d4
coolestProcaviidae <85066178+genericWindowsUser416@users.noreply.github.com>
2025-04-12 19:33:05 +0300

    the page now works as it should

--- Codelabs.DAL/LessonRepository.cs
+++ Codelabs.DAL/LessonRepository.cs
@@ -20,28 +20,12 @@ public class LessonRepository
     public async Task<List<LessonDTO>> GetAllExistingLessonsFromPurchasesByUser(int userID)
     {
         await using var context = new Context();
-        var step1 = context.Purchases.Include(x => x.User);
-
-        var step2 = step1.Where(x => x.User.ID == userID);
-
-        var step3 = step2.Select(p => p.Lesson);
-
-        var step4 = step3.Include(x => x.Author);
-
-        var step5 = step4.Include(x => x.Language);
-
-        var step6 = step5.Where(x => !x.IsDeleted);
-
-        var step7 = await step6.ToListAsync();
-
-        //return await context.Purchases
-        //            .Include(x => x.User)
-        //            .Where(x => x.User.ID == userID)
-        //            .Select(p => p.Lesson)
-        //            .Include(x => x.Author)
-        //            .Include(x => x.Language)
-        //            .Where(x => !x.IsDeleted)
-        //            .ToListAsync();
-        return step7;
+        var lessons = await context.Purchases
+            .Where(p => p.User.ID == userID && !p.Lesson.IsDeleted)
+            .Include(p => p.Lesson)
+                .ThenInclude(l => l.Language)
+            .Select(p => p.Lesson)
+            .ToListAsync();
+        return lessons;
     }
 }
