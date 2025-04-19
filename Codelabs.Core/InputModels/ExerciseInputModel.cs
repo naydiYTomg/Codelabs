4fddffbdecaa6086b7e1b7d2dab0d554d4f529fd
Петр Брякин <petr.bryakin@gmail.com>
2025-04-19 18:18:13 +0300

    finish 84%

--- Codelabs.Core/InputModels/ExerciseInputModel.cs
+++ Codelabs.Core/InputModels/ExerciseInputModel.cs
@@ -6,5 +6,11 @@
         public int? ID { get; set; }
 
         public string Name { get; set; }
+
+        public string DesiredOutput { get; set; }
+
+        public string? ProgramInput { get; set; }
+
+        public string Requirements { get; set; }
     }
 }
