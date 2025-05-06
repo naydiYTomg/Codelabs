b4888b164d23085b5a198bf77019581257686954
coolestProcaviidae <85066178+genericWindowsUser416@users.noreply.github.com>
2025-05-06 22:08:12 +0300

    added lesson's time of creation to DB

--- Codelabs.DAL/Migrations/20250506185732_AddingLessonCreatedTime.Designer.cs
+++ Codelabs.DAL/Migrations/20250506185732_AddingLessonCreatedTime.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Codelabs.DAL;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Codelabs.DAL.Migrations
 {
     [DbContext(typeof(Context))]
-    partial class ContextModelSnapshot : ModelSnapshot
+    [Migration("20250506185732_AddingLessonCreatedTime")]
+    partial class AddingLessonCreatedTime
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Codelabs.DAL/Migrations/20250506185732_AddingLessonCreatedTime.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Codelabs.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddingLessonCreatedTime : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedTime",
+                table: "Lessons",
+                type: "timestamp with time zone",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                            name: "AuthorInfos");
+
+            migrationBuilder.DropTable(
+                name: "Solutions");
+
+            migrationBuilder.DropTable(
+                name: "Exercises");
+
+            migrationBuilder.DropTable(
+                name: "Purchases");
+
+            migrationBuilder.DropTable(
+                name: "Lessons");
+
+            migrationBuilder.DropTable(
+                name: "Languages");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
