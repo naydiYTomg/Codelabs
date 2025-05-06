042ae89837ad6001891fe38b8fb67ad9b6c15db6
coolestProcaviidae <85066178+genericWindowsUser416@users.noreply.github.com>
2025-05-06 18:20:10 +0300

    updated Migrations

---
+++ Codelabs.DAL/Migrations/20250506150850_addinglastviewedlessonstime.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Codelabs.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class addinglastviewedlessonstime : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LastVisitedLessonsPage",
+                table: "Users",
+                type: "timestamp with time zone",
+                nullable: true
+                );
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                            name: "AuthorInfos");
+
+            migrationBuilder.DropTable(
+                name: "Solutions");
+
+            migrationBuilder.DropTable(
+                name: "Exercises");
+
+            migrationBuilder.DropTable(
+                name: "Purchases");
+
+            migrationBuilder.DropTable(
+                name: "Lessons");
+
+            migrationBuilder.DropTable(
+                name: "Languages");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
